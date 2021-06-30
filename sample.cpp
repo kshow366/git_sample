0e5daa690252faf09053c0809d4a3a09229ecee0
keisuke <keisho@toki.waseda.jp>
2021-06-30 19:35:53 +0900

    create sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 
+int sum(int, int);
+
 int main()
 {
-    std::cout<<"Hello World!"<<std::endl;
+    int a = 3; int b = 5;
+    std::cout<<sum(a,b)<<std::endl;
     return 0;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
 }
