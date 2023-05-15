1806b2108ddba2cd4a9291913e17e19d9c86339a
Satoshi0318 <satoshijogo@suou.waseda.jp>
2023-05-15 15:24:52 +0900

    added multiple by SatoshiJogo

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 
-int sum(int, int);
+//int sum(int,int);
+int multiple(int,int);
 
 int main()
 {
     int a = 2; int b = 9;
-    std::cout<<sum(a,b)<<std::endl;
+    //std::cout << sum(a,b) <<std::endl;
+    std::cout << multiple(a,b) <<std::endl;
+
     return 0;
 }
 
-int sum(int a, int b)
+int multiple(int a, int b)
 {
-    return a + b;
+    return a*b;
 }
